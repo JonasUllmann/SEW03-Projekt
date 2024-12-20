771f469f7975002278e1d6eb7cbdbc3f9228996b
JonasUllmann <jonas.ullmann@josephinum.at>
2024-12-20 11:19:08 +0100

    paar klassenerstellt

---
+++ SEW03-Projekt/Classes/ammunition.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEW03_Projekt.Classes
+{
+    public class ammunition
+    {
+
+        public string name;
+        public int radius;
+        public int damage;
+        public int weight;
+
+        public ammunition(string name, int radius, int damage, int weight)
+        {
+            this.name = name;
+            this.radius = radius;
+            this.damage = damage;
+            this.weight = weight;
+        }   
+    }
+}
--- SEW03-Projekt/MainPage.xaml.cs
+++ SEW03-Projekt/MainPage.xaml.cs
@@ -1,14 +1,37 @@
-﻿namespace SEW03_Projekt
+﻿using SEW03_Projekt.Classes;
+
+namespace SEW03_Projekt
 {
     public partial class MainPage : ContentPage
     {
+
+        string playername = "";
+
+        playerobject player1;
+        ammunition apple;
+        ammunition melon;
+        ammunition dung;
         
-        public enum ammo { };
+        public enum Ammo {apple, melon, dung};
 
         public MainPage()
         {
             InitializeComponent();
+
+            player1 = new playerobject(playername, 100, 100);
+            apple = new ammunition("apple", 2, 10, 1);
+            melon = new ammunition("melon", 5, 30, 7);
+            dung = new ammunition("dung", 3, 20, 3);
+
+
+            
+
         }
+
+        
+
+
+
 
 
 
