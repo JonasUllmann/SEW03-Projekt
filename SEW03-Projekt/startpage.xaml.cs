0afeff36efbcc33c68c8bd2a2a9fe1f65383ae72
JonasUllmann <jonas.ullmann@josephinum.at>
2024-12-23 21:10:58 +0100

    tabbedpage erstellt auswählen gegen bot oder gegen 2. SPieler umleitung auf Mainpage

---
+++ SEW03-Projekt/startpage.xaml.cs
@@ -0,0 +1,39 @@
+namespace SEW03_Projekt;
+
+public partial class startpage : TabbedPage
+{
+
+    string botdifficulty = "";
+
+	public startpage()
+	{
+        
+        InitializeComponent();
+	}
+
+    public string Botdifficulty { get => botdifficulty; set => botdifficulty = value; }
+
+    private void btnbot_Clicked(object sender, EventArgs e)
+    {
+        
+        if (entplayername.Text != null && entplayername.Text != "")
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+
+    }
+
+    private void btnplayer_Clicked(object sender, EventArgs e)
+    {
+        Botdifficulty = DifficultyPicker.SelectedItem?.ToString();
+        if (entplayer1name.Text != null && entplayer1name.Text != "")
+        {
+            if (entplayer2name.Text != null && entplayer2name.Text != "")
+            {
+                Navigation.PushAsync(new MainPage());
+            }
+        }
+    }
+
+
+}
